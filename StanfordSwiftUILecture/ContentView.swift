c75f2b1f7566588f52788cac5a7ed871b35d4c40
yoonju lee <rsyjlee@gmail.com>
2022-05-17 08:34:53 +0900

    @main
    * C언어에서 main.c처럼 프로그램의 시작점

---
+++ StanfordSwiftUILecture/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  StanfordSwiftUILecture
+//
+//  Created by 이윤주 on 2022/05/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello, world!")
+
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ StanfordSwiftUILecture/StanfordSwiftUILectureApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordSwiftUILectureApp.swift
+//  StanfordSwiftUILecture
+//
+//  Created by 이윤주 on 2022/05/16.
+//
+
+import SwiftUI
+
+@main // SwiftUI 앱의 시작점
+struct StanfordSwiftUILectureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
