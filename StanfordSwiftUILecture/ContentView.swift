0b9a2c3a64475ef89b0b273d251c4a69a6b1fbd1
yoonju lee <rsyjlee@gmail.com>
2022-05-17 14:30:24 +0900

    Change the number of cards when button tapped
    * Button view -> make view with variable like var body
    * Spacer

--- StanfordSwiftUILecture/ContentView.swift
+++ StanfordSwiftUILecture/ContentView.swift
@@ -9,16 +9,47 @@ import SwiftUI
 
 struct ContentView: View {
     let emojis: [String] = ["✈️", "🚌", "🚟", "🛵", "⛵️", "🚡", "🚗", "🛻", "🚂", "🛳", "🚉", "🚀", "🛶", "🚁", "🚜", "🚁"]
-    var emojiCounts: Int = 6
+    @State var emojiCounts: Int = 6
 
     var body: some View {
-        HStack {
-            ForEach(emojis[0..<emojiCounts], id: \.self) { emoji in
-                CardView(content: emoji)
+        VStack {
+            HStack {
+                ForEach(emojis[0..<emojiCounts], id: \.self) { emoji in
+                    CardView(content: emoji)
+                }
             }
+            .foregroundColor(.yellow)
+            Spacer(minLength: 20)
+            HStack {
+                remove
+                Spacer()
+                add
+            }
+            .font(.largeTitle)
         }
         .padding()
-        .foregroundColor(.blue)
+    }
+    
+    var add: some View {
+        Button {
+            if emojiCounts < emojis.count {
+                emojiCounts += 1
+            }
+        } label: {
+            Image(systemName: "plus.circle")
+        }
+
+    }
+    
+    var remove: some View {
+        Button {
+            if emojis.count > 1 {
+                emojiCounts -= 1
+            }
+        } label: {
+            Image(systemName: "minus.circle")
+        }
+
     }
 }
 
